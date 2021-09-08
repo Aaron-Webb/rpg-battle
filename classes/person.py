6833d5152a7e57b29e7b5eb30bd5923bfa0bdd12
Aaron Webb <avgwebb@outlook.com>
2021-09-08 20:20:43 +0100

    edit person.py and main.py - added functions to get hp stats and choose enemies. Also started game loop with the 1st choice - standard attack

--- classes/person.py
+++ classes/person.py
@@ -1,16 +1,17 @@
+from math import hypot
+import random
+
 class Person:
 
-    def __init__(self, name, hp, mp, attack, defence, magic, items):
+    def __init__(self, name, hp, mp, attack, defence):
         self.name = name
         self.hp = hp
         self.max_hp = hp
         self.mp = mp
         self.max_hp = mp
         self.attacklow = attack - 10
-        self.attachigh = attack + 10
+        self.attackhigh = attack + 10
         self.defence = defence
-        self.magic = magic
-        self.items = items
         self.actions = ["Attack", "Magic", "Items"]
 
 
@@ -24,8 +25,31 @@ class Person:
         return self.hp
 
     # For generating damage. Returns random value between low attack base and high attack ceiling
-    def generate_damage(self):
+    def give_damage(self):
         ''' Generates a random value between the lowest attack value and the
         highest attack valu. Returns this value. This is the damage value taken 
         in by the fucntion take_damage.'''
         return random.randrange(self.attacklow, self.attackhigh)
+
+    # Get players current hp
+    def get_hp(self):
+        return self.hp
+
+    # get players max hp
+    def get_max_hp(self):
+        return self.max_hp
+    
+    
+    def choose_enemy(self, enemies):
+        ''' iterates through enemies and checks the current hp.
+        The player can choose who to attack. '''
+        i = 1
+
+        for enemy in enemies:
+            if enemy.get_hp() != 0:
+                i += 1
+        choice = int(input("Choose enemy: ")) - 1
+        return choice
+
+    
+
--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from classes.game import Person
+from classes.person import Person
 
-#def __init__(self, name, hp, mp, atk, df):
-
-player1 = Person("Cloud", 100, 90, 25, 60)
+# create 3 pla
+player1 = Person("Cloud", 100, 90, 25, 60,)
 player2 = Person("Zidane", 100, 90, 25, 60)
 player3 = Person("Squall", 100, 90, 25, 60)
 
 enemy1 = Person("Sephiroth", 100, 90, 25, 60)
 enemy2 = Person("Ultimecia", 100, 90, 25, 60)
 enemy3 = Person("Aeon", 100, 90, 25, 60)
+
+players = [player1, player2, player3]
+enemies = [enemy1, enemy2, enemy3]
+
+game_on = True
+
+while game_on == True:
+
+    for player in players:
+        player_choice = input("Please choose an action: ")
+        index = int(player_choice) - 1
+
+        # index 0 ("Attack") is a generic attack that gives damage
+        if index == 0:
+            # player gives damage to enemy
+            damage = player.give_damage()
+            enemy = player.choose_enemy(enemies)
+            # enemy takes damage from player
+            enemies[enemy].take_damage(damage)
+            # inform player how much damage has been deal and the enemies current hp
+            print("You attacked " + enemies[enemy].name + " for " + str(damage) + " damage points")
+            print(enemies[enemy].name + " has " + str(enemies[enemy].hp) + " hp left.")
+
+
+        
+        elif index == 1:
+            pass
+
+        
+        elif index == 2:
+            pass
+
+        
+
+
