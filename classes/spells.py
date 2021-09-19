ebfa805b5952714287b731dc6622e4e1769ad621
Aaron Webb <avgwebb@outlook.com>
2021-09-19 12:13:52 +0100

    created the spells class

---
+++ classes/spells.py
@@ -0,0 +1,18 @@
+from math import random
+
+class Spells:
+
+    def __init__(self, name, type, spell_damage, mp_cost):
+        self.name = name
+        self.type = type
+        self.spell_damage = spell_damage
+        self.high_spell_damage = spell_damage + 20
+        self.low_spell_damage = spell_damage - 20
+        self.mp_cost = mp_cost
+
+
+    def give_spell_damage(self):
+        ''' Generated a random value between the lowest
+        spell damage value and the highest spell damage
+        value taken'''
+        return random.randrange(self.low_spell_damage, self.high_spell_damage)
